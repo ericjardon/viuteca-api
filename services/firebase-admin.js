973eab5e7b5ad21ab4837a6773ead9c26c3a89ff
eric chao <ericjardon@hotmail.com>
2022-02-07 22:26:20 -0600

    refactor to a wallet less connection with oracle client 19.14

--- services/firebase-admin.js
+++ services/firebase-admin.js
@@ -30,7 +30,8 @@ function testAuth() {
   .getUser(uid)
   .then((userRecord) => {
     // See the UserRecord reference doc for the contents of userRecord.
-    console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+    console.log(`Successfully fetched user data:`);
+    console.dir(userRecord.toJSON());
   })
   .catch((error) => {
     console.log('Error fetching user data:', error);
